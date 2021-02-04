9db74ab2cfb8f886758506370774ddb97ebe0fb1
Stanislav Dmitriev <dmistas@mail.ru>
2021-02-04 20:34:06 +0500

    added methods for database class

--- Database.php
+++ Database.php
@@ -3,12 +3,12 @@
 class Database
 {
     private $pdo, $query, $error = false, $results, $count;
-    private static $instance;
+    private static $instance = null;
 
     private function __construct()
     {
         try {
-            $this->pdo = new PDO("mysql:host=localhost;dbname=project", "root", "", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+            $this->pdo = new PDO("mysql:host=localhost;dbname=project", "root", "");
         } catch (PDOException $exception) {
             echo "Невозможно установить соединение с БД" . $exception->getMessage();
         }
@@ -22,10 +22,19 @@ class Database
         return self::$instance;
     }
 
-    public function query($sql)
+    public function query($sql, $params = [])
     {
         $this->error = false;
         $this->query = $this->pdo->prepare($sql);
+
+        if (count($params)) {
+            $i = 1;
+            foreach ($params as $param) {
+                $this->query->bindValue($i, $param);
+                $i++;
+            }
+        }
+
         if (!$this->query->execute()) {
             $this->error = true;
         } else {
@@ -33,6 +42,61 @@ class Database
             $this->count = $this->query->rowCount();
         }
         return $this;
+    }
+
+    private function action($action, $table, $where = [])
+    {
+        if (count($where) === 3) {
+
+            $operators = ['<', '<=', '=', '>', '>='];
+
+            $field = $where[0];
+            $operator = $where[1];
+            $value = $where[2];
+
+            if (in_array($operator, $operators)) {
+                $sql = "{$action} FROM {$table} WHERE {$field}  {$operator}  ?";
+                if (!$this->query($sql, [$value])->error()) {
+                    return $this;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function insert($table, $fields = [])
+    {
+        $values = '';
+        foreach ($fields as $item){
+            $values .= "?,";
+        }
+        $sql = "INSERT INTO {$table} (`" . implode('`, `', array_keys($fields)) . "`) VALUES (". rtrim($values, ",") . ")";
+
+        if (!$this->query($sql, $fields)){
+            return true;
+        }
+        return false;
+    }
+
+    public function update($table, $id, $fields=[])
+    {
+        $sql = "UPDATE {$table} SET ". implode('=?,', array_keys($fields)). "=? WHERE id=?";
+        $fields['id']=$id;
+
+        if (!$this->query($sql, $fields)->error()){
+            return true;
+        }
+        return false;
+    }
+
+    public function get($table, $where = [])
+    {
+        return $this->action('SELECT *', $table, $where);
+    }
+
+    public function delete($table, $where)
+    {
+        return $this->action('DELETE', $table, $where);
     }
 
     public function error()
@@ -50,4 +114,13 @@ class Database
         return $this->count;
     }
 
+    /**
+     * Возвращает первый элемент из массива results
+     *
+     * @return mixed
+     */
+    public function first()
+    {
+        return $this->results()[0];
+    }
 }
--- index.php
+++ index.php
@@ -3,16 +3,28 @@ include "Database.php";
 
 $users = Database::getInstance();
 
-$users->query("SELECT * FROM users");
+//$users->get('users', ['id', '=', '42']);
 
-if ($users->error()){
-    echo "we have error";
-    die();
-}
-foreach ($users->results() as $user){
-    echo "<pre>";
-    print_r($user);
-    echo "<pre>";
+//if ($users->error()){
+//    echo "we have error";
+//    die();
+//}
 
-}
-echo $users->count();
+//foreach ($users->results() as $user){
+//    echo "<pre>";
+//    var_dump($user);
+//    echo "<pre>";
+//
+//}
+//echo $users->count();
+//$users->delete('users', ['id', '=', 56]);
+
+//"INSERT INTO users (`name`, `email`, `password`) VALUES (?, ?, ?)";
+$filds = [
+    'name' => 'John Dou',
+    'email' => 'john@mail.com',
+    'password' => '12345'
+];
+
+Database::getInstance()->update('users', 57, $filds);
+//Database::getInstance()->insert('users', $filds);
